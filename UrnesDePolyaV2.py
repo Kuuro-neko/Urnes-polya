b718e11544ab4d672e776d9460b3f474a053ee9b
space-peanut <max.soulie01@laposte.net>
2022-06-03 12:05:16 +0200

    resolution du probleme du code de urnes

--- UrnesDePolyaV2.py
+++ UrnesDePolyaV2.py
@@ -1,22 +1,49 @@
 import random
 import matplotlib.pyplot as plt
 
-def tracerUrnes(nbIt, nbSim, bPiochéAddBleu,bPiochéAddRouge,rPiochéAddBleu, rPiochéAddRouge, operation) :
-    for j in range(nbSim):
-        nbBleu=int(1)
-        nbRouge=int(1)
-        tauxBleuList=list()
-        tauxBleuList.append(100*nbBleu/(nbBleu+nbRouge))
-        for i in range(1,nbIt+1):
-            if
-            num=random.randint(1,nbBleu+nbRouge)
-            if(num<=nbBleu) :
-              nbBleu+=1
-              nbRouge+=1
-            else :
-                nbRouge+=0
-                nbBleu+=1
+class IllegalArgumentError(Exception):
+    pass
+
+def tracerUrnes(nbIt, nbSim, bPiochéAddBleu,bPiochéAddRouge,rPiochéAddBleu, rPiochéAddRouge, operation, nbBstart, nbRstart) :
+    
+   
+    if operation=="add" :
+        for j in range(nbSim):
+            nbBleu=int(nbBstart)
+            nbRouge=int(nbRstart)
+            tauxBleuList=list() 
             tauxBleuList.append(100*nbBleu/(nbBleu+nbRouge))
+            for i in range(1,nbIt+1):
+                num=random.randint(1,nbBleu+nbRouge)
+                if(num<=nbBleu) :
+                  nbBleu+=bPiochéAddBleu
+                  nbRouge+=bPiochéAddRouge
+                else :
+                    nbRouge+=rPiochéAddRouge
+                    nbBleu+=rPiochéAddBleu
+                tauxBleuList.append(100*nbBleu/(nbBleu+nbRouge))
+            plt.plot(tauxBleuList)
+               
+    elif operation=="mult":
+        for j in range(nbSim):
+            nbBleu=int(nbBstart)
+            nbRouge=int(nbRstart)
+            tauxBleuList=list() 
+            tauxBleuList.append(100*nbBleu/(nbBleu+nbRouge))
+            for i in range(1,nbIt+1):
+                num=random.randint(1,nbBleu+nbRouge)
+                if(num<=nbBleu) :
+                  nbBleu*=bPiochéAddBleu
+                  nbRouge*=bPiochéAddRouge
+                else :
+                    nbRouge+=rPiochéAddRouge
+                    nbBleu+=rPiochéAddBleu
+                tauxBleuList.append(100*nbBleu/(nbBleu+nbRouge))
+            plt.plot(tauxBleuList)
+    else :
+        raise IllegalArgumentError("Operation impossible")
+    
         
-        plt.plot(tauxBleuList)
-        tauxBleuesliste=list()
+        
+        
+tracerUrnes(400, 1000, 1, 0, 0, 1, "add", 27,1)
